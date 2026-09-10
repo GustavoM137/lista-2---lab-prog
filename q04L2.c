9e4518581f6d1b3bd2bbfea17855958224cec826
GustavoM137 <gustauvomenezes@gmail.com>
2026-09-10 15:12:39 -0300

    Initialize 'anos' variable to zero

--- q04L2.c
+++ q04L2.c
@@ -2,7 +2,7 @@
 
 int main() {
     float jose_altura = 150, pedro_altura = 110, jose_crescimento = 2, pedro_crescimento = 3;
-    int anos;
+    int anos = 0;
 
     while (jose_altura != pedro_altura) {
         jose_altura += jose_crescimento;
