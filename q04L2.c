76a0e67cad4728030fca90e3b82a743db2e1aad5
GustavoM137 <gustauvomenezes@gmail.com>
2026-09-10 15:13:31 -0300

    Fix loop condition and output message for height comparison
    Corrected the loop condition and adjusted the output message.

--- q04L2.c
+++ q04L2.c
@@ -4,14 +4,14 @@ int main() {
     float jose_altura = 150, pedro_altura = 110, jose_crescimento = 2, pedro_crescimento = 3;
     int anos = 0;
 
-    while (jose_altura != pedro_altura) {
+    while (jose_altura > pedro_altura) {
         jose_altura += jose_crescimento;
         pedro_altura += pedro_crescimento;
 
         anos += 1;
     }
 
-    printf("Pedro será maior que josé em %d anos.\n", anos + 1);
+    printf("Pedro será maior que josé em %d anos.\n", anos);
 
     return 0;
 }
